574fbb818614b6bb191b02e22cc41b78c2662028
tonny62 <ton_kriddanai@hotmail.com>
2018-01-30 23:59:19 +0700

    add see other database

--- database.php
+++ database.php
@@ -1,66 +1,63 @@
 <?php
   require('functions.php');
   require('components.php');
- ?>
+?>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Database</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
-  </head>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Database</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.2/css/bulma.min.css">
+</head>
 
-  <body>
-    <?php navbar(); ?>
-    <section class="section">
-      <div class="container">
-        <div class="columns">
-          <div class="column is-2">
-            <?php sidebar(); ?>
-          </div>
-          <div class="column is-10">
+<body>
+  <?php navbar(); ?>
+  <section class="section">
+    <div class="container is-fluid">
+      <div class="columns">
+        <div class="column is-2">
+          <?php sidebar(); ?>
+        </div>
+        <div class="column is-10">
+          <?php if (!isset($_POST['tag'])): ?>
+            <!-- no tag -->
             <div class="box">
-              <?php if (!isset($_POST['tag'])): ?>
               <div class="columns">
-
+                <div class="column"></div>
                 <div class="column">
-
-                </div>
-                <div class="column">
-                  <form class="" action="database.php" method="post">
+                  <form action="database.php" method="post">
                     <div class="field">
                       <label class="label">Tag</label>
                       <div class="field has-addons">
                         <div class="select">
                           <select name="tag">
-                              <?php
-                              $occlist=['','Computer Occupations','Mathematicians','Engineer','Scientists'];
-                              for ($i=1; $i < 5; $i++) {
-                                  $rows = unserial(getOccupations($i));
-                                  echo("<option value='".$i."'>See All ".$occlist[$i]."</option>");
-                                  foreach ($rows as $key => $value) {
-                                      echo("<option value='".$value['code']."'>".$value['code']." - ".$value['occupation']." ".countoccupation($value['code'])."</option>");
-                                  }
-                              }
-                               ?>
-                            </select>
+                            <?php
+                            $occlist=['','Computer Occupations','Mathematicians','Engineer','Scientists'];
+                            for ($i=1; $i < 5; $i++) {
+                                $rows = unserial(getOccupations($i));
+                                echo("<option value='".$i."'>See All ".$occlist[$i]."</option>");
+                                foreach ($rows as $key => $value) {
+                                    echo("<option value='".$value['code']."'>".$value['code']." - ".$value['occupation']." ".countoccupation($value['code'])."</option>");
+                                }
+                            }
+                            echo("<option value='5'>Other</option>");
+                             ?>
+                          </select>
                         </div>
-                        <input type="submit" class="button" value="Search">
+                        <input type="submit" value="Search" class="button">
                       </div>
                     </div>
                   </form>
-                  <div class="column">
-
-                  </div>
                 </div>
-                <div class="column">
-
-                </div>
+                <div class="column"></div>
               </div>
-              
-                <?php elseif (in_array($_POST['tag'], ['1','2','3','4'])): ?>
-                <div class="content">
-                  <?php
+            </div>
+          <?php else: ?>
+            <!-- selected tag from select -->
+            <?php if (in_array($_POST['tag'],['1','2','3','4'])): ?>
+              <!-- select large category -->
+              <div class="box">
+                <?php
                         if ($_POST['tag'] == 1) {
                             $occu = 'Computer Occupation';
                         } elseif ($_POST['tag'] == 2) {
@@ -71,63 +68,70 @@
                             $occu = 'Scientists';
                         }
                         $rows = getjobsfromcategory($occu);
-                       ?>
-                    <h2><?php echo $occu;?></h2>
-
-                    <?php if (count($rows) == 0): ?>
-                    <h4>No entry</h4>
-                    <?php else: ?>
-                    <div class="table">
-                      <div class="content">
-
-                      </div>
-                      <table>
-                        <thead>
-                          <th>ID</th>
-                          <th>pos</th>
-                          <th>desc</th>
-                          <th>tag</th>
-                          <th>retag</th>
-                        </thead>
-                        <tbody>
-                          <?php
-                                 foreach ($rows as $key => $value) {
-                                     echo "<tr>";
-                                     foreach ($value as $keyin => $valuein) {
-                                         if (in_array($keyin, ['_id','pos','desc'])) {
-                                             echo "<td>";
-                                             echo $valuein;
-                                             echo "</td>";
-                                         }
-                                     }
-                                     // var_dump($value);
-                                     echo "<td>".getcategoryname($value['code'])."</td>";
-                                     echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
-                                     echo "</tr>";
-                                 }
-                               ?>
-                        </tbody>
-                      </table>
-                    </div>
-                    <?php endif; ?>
-
-                </div>
-
-                <?php else: ?>
+                ?>
                 <div class="content">
-                  <?php
-                      echo "<h2>".getcategoryname($_POST['tag'])."</h2>";
-                      $rows = getjobsfromtag($_POST['tag']);
-                     ?>
-                    <?php if (count($rows) == 0): ?>
-                    <h2>No Entry</h2>
-
+                  <h2><?php echo $occu;?></h2>
                 </div>
+                <?php if (count($rows) == 0): ?>
+                  <!-- no rows -->
+                  <h4>No entry</h4>
                 <?php else: ?>
-                <div class="table">
-                  <div class="content">
+                  <!-- has rows -->
+                  <div class="table">
+                    <table>
+                      <thead>
+                        <th>ID</th>
+                        <th>pos</th>
+                        <th>desc</th>
+                        <th>tag</th>
+                        <th>retag</th>
+                      </thead>
+                      <tbody>
+                        <?php
+                         foreach ($rows as $key => $value) {
+                             echo "<tr>";
+                             echo "<td>".$value['_id']."</td>";
+                             if (!isset($value['pos'])) {
+                               echo "<td></td>";
+                             }else {
+                               echo "<td>".$value['pos']."</td>";
+                             }
+                             echo "<td>".$value['desc']."</td>";
 
+                             // foreach ($value as $keyin => $valuein) {
+                             //     if (in_array($keyin, ['_id','pos','desc'])) {
+                             //         echo "<td>";
+                             //         echo $valuein;
+                             //         echo "</td>";
+                             //     }
+                             // }
+                             // var_dump($value);
+
+                             if (isset($value['code'])) {
+                               echo "<td>".getcategoryname($value['code'])."</td>";
+                             }else {
+                               echo "<td>Marked</td>";
+                             }
+
+                             echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
+                             echo "</tr>";
+                         }
+                       ?>
+                      </tbody>
+                    </table>
                   </div>
+                </div>
+              <?php endif; ?>
+            <?php elseif($_POST['tag'] == '5'): ?>
+              <!-- select other -->
+              <div class="box">
+                <?php
+                  $rows = getjobsfromcategory('Other');
+                ?>
+                <div class="content">
+                  <h2>Other</h2>
+                </div>
+                <div class="table">
                   <table>
                     <thead>
                       <th>ID</th>
@@ -137,33 +141,75 @@
                     </thead>
                     <tbody>
                       <?php
-                                foreach ($rows as $key => $value) {
-                                    echo "<tr>";
-                                    foreach ($value as $keyin => $valuein) {
-                                        if (in_array($keyin, ['_id','pos','desc'])) {
-                                            echo "<td>";
-                                            echo $valuein;
-                                            echo "</td>";
-                                        }
-                                    }
-                                    // var_dump($value);
-                                    echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
-                                    echo "</tr>";
-                                }
-                              ?>
+                       foreach ($rows as $key => $value) {
+                           echo "<tr>";
+                           echo "<td>".$value['_id']."</td>";
+                           if (!isset($value['pos'])) {
+                             echo "<td></td>";
+                           }else {
+                             echo "<td>".$value['pos']."</td>";
+                           }
+                           if (!isset($value['desc'])) {
+                             echo "<td></td>";
+                           }else {
+                             echo "<td>".$value['desc']."</td>";
+                           }
+                           echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
+                           echo "</tr>";
+                       }
+                     ?>
                     </tbody>
                   </table>
                 </div>
-                <?php endif; ?>
-
-
-                <?php endif; ?>
-
               </div>
+            <?php else: ?>
+              <!-- select occupation -->
+              <div class="box">
+                <div class="content">
+                  <?php
+                        echo "<h2>".getcategoryname($_POST['tag'])."</h2>";
+                        $rows = getjobsfromtag($_POST['tag']);
+                  ?>
+                </div>
+                <?php if (count($rows) == 0): ?>
+                  <!-- no rows -->
+                  <h4>No entry</h4>
+                <?php else: ?>
+                  <!-- rows -->
+                  <div class="table">
+                    <table>
+                      <thead>
+                        <th>ID</th>
+                        <th>pos</th>
+                        <th>desc</th>
+                        <th>retag</th>
+                      </thead>
+                      <tbody>
+                        <?php
+                          foreach ($rows as $key => $value) {
+                              echo "<tr>";
+                              foreach ($value as $keyin => $valuein) {
+                                  if (in_array($keyin, ['_id','pos','desc'])) {
+                                      echo "<td>";
+                                      echo $valuein;
+                                      echo "</td>";
+                                  }
+                              }
+                              // var_dump($value);
+                              echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
+                              echo "</tr>";
+                          }
+                        ?>
+                      </tbody>
+                    </table>
+                  </div>
 
-            </div>
-          </div>
+                <?php endif; ?>
+              </div>
+            <?php endif; ?>
+          <?php endif; ?>
         </div>
       </div>
-    </section>
-  </body>
+    </div>
+  </section>
+</body>
---
+++ database_old.php
@@ -0,0 +1,162 @@
+
+
+
+
+  <body>
+    <?php navbar(); ?>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-2">
+            <?php sidebar(); ?>
+          </div>
+          <div class="column is-10">
+            <div class="box">
+              <?php if (!isset($_POST['tag'])): ?>
+              <div class="columns">
+
+                <div class="column">
+
+                </div>
+                <div class="column">
+                  <form class="" action="database.php" method="post">
+                    <div class="field">
+                      <label class="label">Tag</label>
+                      <div class="field has-addons">
+                        <div class="select">
+                          <select name="tag">
+                              <?php
+                              $occlist=['','Computer Occupations','Mathematicians','Engineer','Scientists'];
+                              for ($i=1; $i < 5; $i++) {
+                                  $rows = unserial(getOccupations($i));
+                                  echo("<option value='".$i."'>See All ".$occlist[$i]."</option>");
+                                  foreach ($rows as $key => $value) {
+                                      echo("<option value='".$value['code']."'>".$value['code']." - ".$value['occupation']." ".countoccupation($value['code'])."</option>");
+                                  }
+                              }
+                              echo("<option value='5'>Other</option>");
+                               ?>
+                            </select>
+                        </div>
+                        <input type="submit" class="button" value="Search">
+                      </div>
+                    </div>
+                  </form>
+                  <div class="column">
+
+                  </div>
+                </div>
+                <div class="column">
+
+                </div>
+              </div>
+              <?php elseif (in_array($_POST['tag'], ['1','2','3','4'])): ?>
+              <div class="content">
+                <?php
+                        if ($_POST['tag'] == 1) {
+                            $occu = 'Computer Occupation';
+                        } elseif ($_POST['tag'] == 2) {
+                            $occu = 'Mathematicians';
+                        } elseif ($_POST['tag'] == 3) {
+                            $occu = 'Engineer';
+                        } else {
+                            $occu = 'Scientists';
+                        }
+                        $rows = getjobsfromcategory($occu);
+                       ?>
+                  <h2><?php echo $occu;?></h2>
+
+                  <?php if (count($rows) == 0): ?>
+                  <h4>No entry</h4>
+                  <?php else: ?>
+                  <div class="table">
+                    <div class="content">
+
+                    </div>
+                    <table>
+                      <thead>
+                        <th>ID</th>
+                        <th>pos</th>
+                        <th>desc</th>
+                        <th>tag</th>
+                        <th>retag</th>
+                      </thead>
+                      <tbody>
+                        <?php
+                                 foreach ($rows as $key => $value) {
+                                     echo "<tr>";
+                                     foreach ($value as $keyin => $valuein) {
+                                         if (in_array($keyin, ['_id','pos','desc'])) {
+                                             echo "<td>";
+                                             echo $valuein;
+                                             echo "</td>";
+                                         }
+                                     }
+                                     // var_dump($value);
+                                     echo "<td>".getcategoryname($value['code'])."</td>";
+                                     echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
+                                     echo "</tr>";
+                                 }
+                               ?>
+                      </tbody>
+                    </table>
+                  </div>
+                  <?php endif; ?>
+
+              </div>
+
+              <?php else: ?>
+              <!-- other -->
+              <div class="content">
+                <?php
+                      echo "<h2>".getcategoryname($_POST['tag'])."</h2>";
+                      $rows = getjobsfromtag($_POST['tag']);
+                     ?>
+                  <?php if (count($rows) == 0): ?>
+                  <h2>Other</h2>
+
+              </div>
+              <?php else: ?>
+              <div class="table">
+                <div class="content">
+
+                </div>
+                <table>
+                  <thead>
+                    <th>ID</th>
+                    <th>pos</th>
+                    <th>desc</th>
+                    <th>retag</th>
+                  </thead>
+                  <tbody>
+                    <?php
+                                foreach ($rows as $key => $value) {
+                                    echo "<tr>";
+                                    foreach ($value as $keyin => $valuein) {
+                                        if (in_array($keyin, ['_id','pos','desc'])) {
+                                            echo "<td>";
+                                            echo $valuein;
+                                            echo "</td>";
+                                        }
+                                    }
+                                    // var_dump($value);
+                                    echo "<td><a href='retag.php?jobid=".$value['_id']."' class='button is-link'>Retag</a></td>";
+                                    echo "</tr>";
+                                }
+                              ?>
+                  </tbody>
+                </table>
+              </div>
+              <?php endif; ?>
+
+
+              <?php endif; ?>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+      </div>
+    </section>
+  </body>
